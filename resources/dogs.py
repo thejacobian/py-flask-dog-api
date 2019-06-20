a4e7232e94328a373c6c76ffc7344d63a3519715
Jake Shelton <jake.s.shelton@gmail.com>
2019-06-20 14:22:11 -0600

    added dog GET, POST, and PUT routes

--- resources/dogs.py
+++ resources/dogs.py
@@ -5,66 +5,124 @@ from flask_restful import (Resource, Api, reqparse, fields, marshal,
 
 import models
 
-## define fields on responses
+## Marshal fields on responses
 dog_fields = {
     'id': fields.Integer,
     'name': fields.String,
     'breed': fields.String,
+    'owner': fields.String,
 }
 
 
 class DogList(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument(
-            'name',
-            required=False,
-            help='No dog name provided',
-            location=['form', 'json']
-        )
-        self.reqparse.add_argument(
-            'breed',
-            required=False,
-            help='No course dog breed provided',
-            location=['form', 'json']
-        )
-        self.reqparse.add_argument(
-            'owner',
-            required=False,
-            help='No  owner provided',
-            location=['form', 'json']
-        )
-        super().__init__()
-        
-    def get(self):
-        return jsonify({'dogs': [{'name': 'Archie'}]})
+  def __init__(self):
+    self.reqparse = reqparse.RequestParser()
+    self.reqparse.add_argument(
+      'name',
+      required=False,
+      help='No dog name provided',
+      location=['form', 'json']
+    )
+    self.reqparse.add_argument(
+      'breed',
+      required=False,
+      help='No dog breed provided',
+      location=['form', 'json']
+    )
+    self.reqparse.add_argument(
+      'owner',
+      required=False,
+      help='No owner provided',
+      location=['form', 'json']
+    )
+    super().__init__()
+      
+@marshal_with(dog_fields)
+def get(self, id):
+  try:
+    dog = models.Dog.get(models.Dog.id==id)
+  except models.Dog.DoesNotExist:
+    abort(404)
+  else:
+    return (dog, 200)
+  # return jsonify({'dogs': [{'name': 'Archie'}]})
 
-    def post(self):
-        args = self.reqparse.parse_args()
-        print(args, 'hittingggg')
-        dog = models.Dog.create(**args)
-        return jsonify({'dogs': [{'name': 'Archie'}]})
+@marshal_with(dog_fields)
+def put(self, id):
+  args = self.reqparse.parse_args()
+  query = models.Dog.update(**args).where(models.Dog.id==id)
+  query.execute()
+  print(query, "<-- this is query")
+  return (models.Dog.get(models.Dog.id==id), 200)
 
+@marshal_with(dog_fields)
+def post(self):
+  args = self.reqparse.parse_args()
+  print(args, 'hittingggg post')
+  dog = models.Dog.create(**args)
+  return dog
 
 class Dog(Resource):
-    def get(self, id):
-        return jsonify({'name': 'Archie'})
+  def __init__(self):
+    self.reqparse = reqparse.RequestParser()
+    self.reqparse.add_argument(
+      'name',
+      required=False,
+      help='No dog name provided',
+      location=['form', 'json']
+    )
+    self.reqparse.add_argument(
+      'breed',
+      required=False,
+      help='No dog breed provided',
+      location=['form', 'json']
+    )
+    self.reqparse.add_argument(
+      'owner',
+      required=False,
+      help='No owner provided',
+      location=['form', 'json']
+    )
 
-    def put(self, id):
-        return jsonify({'name': 'Archie'})
+    super().__init__()
+
+  @marshal_with(dog_fields)
+  def get(self, id):
+    try:
+      dog = models.Dog.get(models.Dog.id==id)
+    except models.Dog.DoesNotExist:
+      abort(404)
+    else:
+      return (dog, 200)
+    # return jsonify({'dogs': [{'name': 'Archie'}]})
+
+  @marshal_with(dog_fields)
+  def put(self, id):
+    args = self.reqparse.parse_args()
+    query = models.Dog.update(**args).where(models.Dog.id==id)
+    query.execute()
+    print(query, "<-- this is query")
+    return (models.Dog.get(models.Dog.id==id), 200)
+
+  @marshal_with(dog_fields)
+  def post(self):
+    args = self.reqparse.parse_args()
+    print(args, 'hittingggg post')
+    dog = models.Dog.create(**args)
+    return dog
 
     def delete(self, id):
-        return jsonify({'name': 'Archie'})
+      return jsonify({'name': 'Archie'})
 
 dogs_api = Blueprint('resources.dogs', __name__)
 api = Api(dogs_api)
 api.add_resource(
-    DogList,
-    '/dogs',
-    endpoint='dogs'
+  DogList,
+  '/dogs',
+  endpoint='dogs'
 )
 api.add_resource(
-    Dog,
-    '/dogs/<int:id>',
-    endpoint='dog'
+  Dog,
+  '/dogs/<int:id>',
+  endpoint='dog'
 )
